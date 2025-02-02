f6ad40997ee3d473faa75ab69bb33474b1d09e8c
DocDrag <78905953+DocDrag@users.noreply.github.com>
2025-02-02 13:07:04 +0700

    first commit

---
+++ TURBO_C.h
@@ -0,0 +1,11 @@
+/**
+ * This code works on Windows and has been tested using Dev C++ and Code::Blocks only.
+ * Tested with GNU GCC Compiler.
+ * Some functions may not be fully compatible with the Turbo C++ environment.
+ * The code uses Windows APIs for console manipulation and ANSI escape codes.
+ * Please open Dev C++ and Code::Blocks with Run as Administrator for better performance.
+ * This file is not part of the mingw-w64 runtime package.
+ */
+
+#ifndef TURBO_C_H
+#define TURBO_C_H
